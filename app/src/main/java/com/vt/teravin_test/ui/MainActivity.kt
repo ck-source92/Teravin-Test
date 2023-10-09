0d2c4e2858740873fac1e1fea8ea67d3e1568b38
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-10-09 13:01:44 +0700

    add splash screen

--- app/src/main/java/com/vt/teravin_test/ui/MainActivity.kt
+++ app/src/main/java/com/vt/teravin_test/ui/MainActivity.kt
@@ -15,8 +15,8 @@ import com.vt.teravin_test.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
